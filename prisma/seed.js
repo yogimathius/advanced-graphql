c5be13f9c4bf1ebbd7ad7c65fa8f99189d5cc8df
Mathius <info@yogimathius.dev>
2023-09-11 21:45:22 -0700

    chore: add prisma

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,78 +1,56 @@
 const { PrismaClient } = require("@prisma/client");
-
 const prisma = new PrismaClient();
 
-async function seed() {
-  // Create users
-  const postLikedByUser1 = {
-    data: {
-      title: "Seeking advice on home improvement projects 🏠",
-      description:
-        "Looking to renovate my kitchen. Any DIY tips? #HomeImprovement",
-    },
-  };
-
-  const user1 = await prisma.user.create({
-    data: {
-      name: "Helpful Hannah",
-      email: "hannah@example.com",
-      password: "supportive1",
-      posts: {
-        create: [
-          {
-            title: "Offering tech help for coding questions 🖥️",
-            description:
-              "If you're stuck on a coding problem, I'm here to help! #CodingHelp",
-          },
-          {
-            title: "Looking for gardening advice 🌱",
-            description:
-              "I'm a newbie gardener and could use some tips. #Gardening",
-          },
-          postLikedByUser1,
-        ],
+async function main() {
+  try {
+    // Insert users
+    const user1 = await prisma.user.create({
+      data: {
+        name: "Helpful Hannah",
+        email: "hannah@example.com",
+        password: "supportive1",
+        posts: {
+          create: [
+            {
+              title: "Offering tech help for coding questions 🖥️",
+              description:
+                "If you're stuck on a coding problem, I'm here to help! #CodingHelp",
+            },
+            {
+              title: "Looking for gardening advice 🌱",
+              description:
+                "I'm a newbie gardener and could use some tips. #Gardening",
+            },
+          ],
+        },
       },
-    },
-  });
+    });
 
-  const postLikedByUser2 = {
-    data: {
-      title: "Looking for a captivating read. Any suggestions? 📚",
-      description: "Liked 1 time",
-    },
-  };
-
-  const user2 = await prisma.user.create({
-    data: {
-      name: "Resourceful Rob",
-      email: "rob@example.com",
-      password: "problemSolver2",
-      posts: {
-        create: [
-          {
-            title: "Offering car repair expertise 🚗",
-            description: "Experienced mechanic ready to assist! #CarRepair",
-          },
-          postLikedByUser2,
-        ],
+    const user2 = await prisma.user.create({
+      data: {
+        name: "Resourceful Rob",
+        email: "rob@example.com",
+        password: "problemSolver2",
+        posts: {
+          create: [
+            {
+              title: "Offering car repair expertise 🚗",
+              description: "Experienced mechanic ready to assist! #CarRepair",
+            },
+          ],
+        },
       },
-    },
-  });
+    });
 
-  // Create posts liked by users
-
-  // Create likes
-  await prisma.like.create({
-    data: {
-      user: user2,
-      post: postLikedByUser2,
-    },
-  });
-
-  console.log("Seed data created:", { user1, user2 });
+    console.log("Seed data created:", { user1, user2 });
+  } catch (error) {
+    console.error("Error seeding data:", error);
+  } finally {
+    await prisma.$disconnect();
+  }
 }
 
-seed()
+main()
   .catch((e) => {
     throw e;
   })
